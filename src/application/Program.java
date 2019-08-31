51b50c499470351f64bea4ad59240590d1598070
cristianomaf <cristianomaf@live.com>
2019-08-31 10:33:07 -0300

    usando o removeIf

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Produto;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Produto> lista = new ArrayList<>();
+		
+		lista.add(new Produto("TV",900.00));
+		lista.add(new Produto("Mouse",50.00));
+		lista.add(new Produto("Tablet",350.50));
+		lista.add(new Produto("HD Case",80.90));
+		
+		//remove da lista produtos com preco acima de 100
+		lista.removeIf(p->p.getPreco()>=100);
+		
+		//percorre a lista imprimindo produtos
+		for(Produto p: lista) {
+			System.out.println(p);
+		}
+
+	}
+
+}
